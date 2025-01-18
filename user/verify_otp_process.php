e3b9570cf21a53af4217e7924fd56004f7f1d24c
ansariasfak955 <95132739+ansariasfak955@users.noreply.github.com>
2025-01-18 12:43:13 +0530

    Update verify_otp_process.php

--- user/verify_otp_process.php
+++ user/verify_otp_process.php
@@ -126,7 +126,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $mail->Port = 587;
 
             // Recipients
-            $mail->setFrom('ansariasfak955@gmail.com', 'Testing Info');
+            $mail->setFrom('admin@gmail.com', 'Testing Info');
             $mail->addAddress($email);
 
             // Attachments
