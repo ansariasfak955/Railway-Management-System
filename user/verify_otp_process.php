b7ea12fb6b59cb97e0d495aaee79a0ed8041feeb
ansariasfak955 <95132739+ansariasfak955@users.noreply.github.com>
2025-01-18 12:42:47 +0530

    Update verify_otp_process.php

--- user/verify_otp_process.php
+++ user/verify_otp_process.php
@@ -120,8 +120,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'ansariasfak955@gmail.com';
-            $mail->Password = 'hotj qqbk kxaf dqkq'; // Gmail App Password
+            $mail->Username = 'admin@gmail.com';
+            $mail->Password = ''; // Gmail App Password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
